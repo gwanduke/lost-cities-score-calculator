d97dc076792312125a791d8d5a880b15362e3975
gwanduke <gwanduke@gmail.com>
2020-12-24 06:12:15 +0900

    Add ts-jest

--- src/models/Board.test.ts
+++ src/models/Board.test.ts
@@ -2,11 +2,13 @@ import Board from "./Board";
 
 test("", () => {
   const board = Board.create({
+    id: "",
     colors: ["red", "blue", "yellow"],
     isSixthExpedition: false,
   });
 
   expect(board).toEqual({
+    id: "",
     colors: ["red", "blue", "yellow"],
     isSixthExpedition: false,
   });
